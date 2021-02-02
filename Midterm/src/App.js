3fff14a4e140cdbfe0182fe604925eda924e8479
= <=>
2021-02-01 23:53:25 -0800

    sidemenu interaction, contentbox database

--- Midterm/src/App.js
+++ Midterm/src/App.js
@@ -1,14 +1,10 @@
 import './App.scss';
-// import Main from 'pages/main';
-import Button from 'comps/Button'
-import Status from 'comps/Status'
+import Main from 'pages/main'
 
 function App() {
   return (
     <div className="App">
-      {/* <Main /> */}
-      <Button />
-      <Status />
+      <Main />
     </div>
   );
 }
---
+++ Midterm/src/pages/ContentDetail/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Sidemenu from 'comps/sidemenu';
+import TopBar from 'comps/Topbar';
+
+const ContentDetail = () => {
+    return <div className="Content-detail">
+        <TopBar/>
+    </div>
+}
+
+export default ContentDetail;
