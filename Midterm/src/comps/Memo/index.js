de14c3bb32dd382bafde52a40f08bf5cfb175761
叶伊婧 <yeyijing@yeyijingdeMacBook-Pro.local>
2021-02-01 19:54:22 -0800

    Memo part

--- Midterm/src/comps/Memo/index.js
+++ Midterm/src/comps/Memo/index.js
@@ -2,18 +2,33 @@ import React from 'react';
 import styled from 'styled-components';
 
 const MemoContainer = styled.div`
-   
+position: absolute;
 `;
 
+const MemoText = styled.div`
+font-family: Roboto;
+font-style: normal;
+font-weight: bold;
+font-size: 20px;
+line-height: 23px;
+color: #000000;
+`
+const MemoInput = styled.input`
 
-const Memo =() => {
+border: none;
+border-bottom:gray 1px solid;  
+`
+
+
+const Memo =({border}) => {
     return <MemoContainer>
-
+       <MemoText>Memo</MemoText>
+       <MemoInput type='text' placeholder='Leave Memo' size='50'></MemoInput>
     </MemoContainer>
 }
 
 Memo.defaultProps = {
-
+   
 }
 
 export default Memo
