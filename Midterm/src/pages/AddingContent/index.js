f89bd11c81fd71a0392b4deac39dc86871efda1f
Kai Erfle <kaierfle@gmail.com>
2021-02-03 16:45:13 -0800

    fun gradients to home page :)

--- Midterm/src/pages/AddingContent/index.js
+++ Midterm/src/pages/AddingContent/index.js
@@ -1,11 +1,11 @@
-import React, {useState, useEffect} from 'react';
-import TopBar from 'comps/Topbar';
-import InputComp from 'comps/Input';
-import Status from 'comps/Status';
-import Platform from 'comps/Platform';
-import Memo from 'comps/Memo';
-import Button from 'comps/Button';
 import axios from 'axios';
+import Button from 'comps/Button';
+import InputComp from 'comps/Input';
+import Memo from 'comps/Memo';
+import Platform from 'comps/Platform';
+import Status from 'comps/Status';
+import TopBar from 'comps/Topbar';
+import React, { useState } from 'react';
 
 
 const AddContent = () => {
@@ -19,88 +19,102 @@ const AddContent = () => {
     const [memo, setMemo] = useState("");
     const [selected, setSelected] = useState(false);
 
-    const HandleAdding = async(title, director, status, platform, year, img, memo) => {
+    const HandleAdding = async (title, director, status, platform, year, img, memo) => {
         let resp = await axios.post("/api/movies", {
             title: title,
             director: director,
             status: status,
             platform: platform,
             year: year,
-            img:img,
-            memo:memo
-            
+            img: img,
+            memo: memo
+
         })
         console.log(resp);
     }
 
     return <div className="add-content">
-        <TopBar/>
-        <InputComp inputtitle={"Content Name"} catchInput={(e)=>{
+        <TopBar />
+        <InputComp inputtitle={"Content Name"} catchInput={(e) => {
             setTitle(e.target.value)
-        }}/>
+        }} />
         <div className="contnet-infomation">
-            <InputComp inputtitle={"Director"} catchInput={(e)=>{
+            <InputComp inputtitle={"Director"} catchInput={(e) => {
                 setDirector(e.target.value)
-            }}/>
+            }} />
 
-            <InputComp inputtitle={"Year"} catchInput={(e)=>{
+            <InputComp inputtitle={"Year"} catchInput={(e) => {
                 setYear(e.target.value)
-            }}/>   
+            }} />
         </div>
         <h2>Status</h2>
         <div className="status">
-            <Status selected={selected} onClick={()=>{
-                setStatus("watching")
-                setSelected(!selected)
-            }} text="Watching" bg="watching"/>
-            <Status selected={selected} onClick={()=>{
-                setStatus("watched")
-                setSelected(!selected)
-            }} text="Watched" bg="watched"/>
-            <Status onClick={()=>{
-                setStatus("waiting")
-            }} text="Waiting" bg="waiting"/>
-            <Status onClick={()=>{
-                setStatus("stopped")
-            }} text="Stopped" bg="stopped"/>
+            <table>
+                <tr>
+                    <td>
+                        <Status className="btn" selected={selected} onClick={() => {
+                            setStatus("watching")
+                            setSelected(!selected)
+                        }} text="Watching" bg="watching" />
+                    </td>
+                    <td>
+                        <Status className="btn" selected={selected} onClick={() => {
+                            setStatus("watched")
+                            setSelected(!selected)
+                        }} text="Watched" bg="watched" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <Status className="btn" onClick={() => {
+                            setStatus("waiting")
+                        }} text="Waiting" bg="waiting" />
+                    </td>
+                    <td>
+                        <Status className="btn" onClick={() => {
+                            setStatus("stopped")
+                        }} text="Stopped" bg="stopped" />
+                    </td>
+                </tr>
+            </table>
         </div>
         <h2>Platform</h2>
         <div className="platform">
             <table>
                 <tr>
-                    <td> 
-                    <Platform onClick={()=>{
-                        setPlatform("Netflix")
-                    }} text="Netflix" bg="netflix" color="black"/>
+                    <td>
+                        <Platform className="btn" onClick={() => {
+                            setPlatform("Netflix")
+                        }} text="Netflix" bg="netflix" color="black" />
                     </td>
                     <td>
-                    <Platform onClick={()=>{
-                        setPlatform("Disney+")
-                    }} text="Disney +" bg="disney"/>
+                        <Platform className="btn" onClick={() => {
+                            setPlatform("Disney+")
+                        }} text="Disney +" bg="disney" />
 
                     </td>
                 </tr>
                 <tr>
                     <td>
-                    <Platform onClick={()=>{
-                        setPlatform("Amazon")
-                    }} text="Amazon" bg="amazon" color="black"/>
+                        <Platform className="btn" onClick={() => {
+                            setPlatform("Amazon")
+                        }} text="Amazon" bg="amazon" color="black" />
                     </td>
-                    <td> 
-                    <Platform onClick={()=>{
-                        setPlatform("Hulu")
-                    }} text="Hulu" bg="hulu" color="black"/>
+                    <td>
+                        <Platform className="btn" onClick={() => {
+                            setPlatform("Hulu")
+                        }} text="Hulu" bg="hulu" color="black" />
 
                     </td>
                 </tr>
             </table>
         </div>
-        <Memo catchMemo={(e)=>{
+        <Memo catchMemo={(e) => {
             setMemo(e.target.value)
-        }}/>
-        <Button onClick={()=>{
+        }} />
+        <Button onClick={() => {
             HandleAdding(title, director, status, platform, year, img)
-        }} text="Add"/>
+        }} text="Add" />
 
     </div>
 }
